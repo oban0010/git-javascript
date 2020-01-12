3040f5971ccaa9f334b7691ccc6a2dd8b15227e1
David Obando <oban0010@algonquinlive.com>
2020-01-12 15:39:13 +0500

    Adding index.html and script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+var name = "David Obando";
+console.log("Hello, my name is " + name);
+
+var number = 9;
+var mulby2 = number * 2 ;
+
+console.log( "Number Multiply by 2 is " + mulby2);
+
+var resby5 = mulby2 * 5;
+console.log("Result Multiply by 5 is " + resby5);
+
+var divbynum = resby5/number;
+console.log("Result Divided by Number("+number+") is " + divbynum);
+
+var sub7 = divbynum - 7;
+console.log("Subtract 7 from Result is " + sub7);
